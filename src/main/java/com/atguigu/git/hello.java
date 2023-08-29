cc4788a22d5e55f3d3dc5371b940fb25207210e0
ys <910949638@qq.com>
2023-08-29 15:31:58 +0800

    mergeMaster1

--- src/main/java/com/atguigu/git/hello.java
+++ src/main/java/com/atguigu/git/hello.java
@@ -6,6 +6,6 @@ public class hello {
         System.out.println("change1");
         System.out.println("change2");
         System.out.println("hotfix");
-        System.out.println("master");
+        System.out.println("master test");
     }
 }
